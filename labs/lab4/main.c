4e0f2fd1278c6176cf51b967ddec93c5954cd0a0
Jarett <jarettn18@gmail.com>
2020-10-30 16:40:55 -0700

    Lab 4 complete

--- labs/lab4/main.c
+++ labs/lab4/main.c
@@ -43,26 +43,31 @@ int main(int argc, char *argv[])
 {
 	if (argc < 2) {
 		fprintf(stderr, "Error: unspecified amount of child processes\n");
-		fprintf(stderr, "Usage: ./a.out <amount of child processes\n");
+		fprintf(stderr, "Usage: ./a.out <amount of child processes>\n");
 		exit(-1);
 	}
 
 	int n = atoi(argv[1]);
+	int parent_pid = getpid();
 
 	pid_t pid_array[n];
+	
+	//Step 3 and 4
+	for (int i = 0 ; i < n ; i++) {
+		if (getpid() == parent_pid){
+			pid_array[i] = fork();
+		}
+		if (pid_array[i] == 0){
+			execlp("./iobound", "./iobound", "-seconds", "5");
+		}
+	}
+	
+	script_print(pid_array, n);
 
 	for (int i = 0 ; i < n ; i++) {
-		pid_array[i] = fork();
-		printf("my pid = %d, my parent = %d\n",getpid(), getppid());
-		execlp("./iobound", "./iobound", "-seconds", "5");
-	}
-
-	script_print(pid_array, 5);
-
-	for (int i = 0 ; i < n ; i++)
-	{
 		wait(0);
 	}
+
 	//Step 1 and 2
 	//pid_t child_pid = fork();
 	//execlp("./iobound", "./iobound", "-seconds", "5");
